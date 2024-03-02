e18c8bd1d0e97a94ddce599f49ac25d577393d0a
ahdbot <ahdbot3@gmail.com>
2024-03-02 19:12:34 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+import 'bootstrap/dist/css/bootstrap.rtl.css';
+require('bootstrap/dist/js/bootstrap.bundle');
+
+
+import "@fortawesome/fontawesome-free/js/all.min";
+import "./css/style.css";
+import './sass/style.scss'
---
+++ webpack.config.js
@@ -0,0 +1,92 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const path = require('path');
+module.exports = {
+  mode: "development",
+  entry: {
+    app: "./src/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    hot: false,
+    port: 65535,
+    open: true,
+    devMiddleware: {
+      writeToDisk: true,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+        options: {
+          minimize: true,
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        exclude: /custom\.scss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../',
+              esModule: false,
+            },
+          },
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      // {
+      //   test: /custom\.scss$/i,
+      //   use: [
+      //     {
+      //       loader: MiniCssExtractPlugin.loader,
+      //       options: {
+      //         // esModule: false,
+      //         publicPath: '../'
+      //       },
+      //     },
+      //     "rtlcss-loader",
+      //     "sass-loader"
+      //   ],
+      // },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: "./images/[name][ext]"
+        }
+      },
+      {
+        test: /\.(svg|eot|woff|woff2|ttf)$/i,
+
+        type: 'asset/resource',
+        generator: {
+          filename: "./fonts/[name][ext]"
+        }
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+        filename: "index.html",
+        template: "./src/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/style.css"
+    }),
+
+  ]
+  
+};
