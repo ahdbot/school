2e3a51d958d795b200f486e4d8549e654cf410f0
ahdbot <ahdbot3@gmail.com>
2024-03-03 20:01:06 +0300

    student section finish

--- src/index.js
+++ src/index.js
@@ -7,3 +7,14 @@ import "@fortawesome/fontawesome-free/js/all.min";
 import "./css/style.css";
 import './sass/style.scss'
 // import "jquery/dist/jquery.js";
+
+
+
+function scrollFnuction()
+{
+    if (document.documentElement.scrollTop > 200) {
+        document.getElementById('navbar').classList.add('noTransparrent');
+    } else {
+        document.getElementById('navbar').classList.remove('noTransparrent');
+    }
+};
