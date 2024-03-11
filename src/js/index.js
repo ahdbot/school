cf5cb91e2701adee826f3a87453ca5b93601cdda
ahdbot <ahdbot3@gmail.com>
2024-03-11 19:57:40 +0300

    finish edit

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 
 import 'bootstrap/dist/css/bootstrap.rtl.css';
 require('bootstrap/dist/js/bootstrap.bundle');
-import { CountUp } from 'countup.js';
 import "@fortawesome/fontawesome-free/js/all.min";
 import '../sass/style.scss'
 import '../sass/register.scss'
@@ -13,6 +12,10 @@ import '../sass/student.scss'
 import Swiper from 'swiper/bundle';
 
 import 'swiper/css/bundle';
+
+
+import { CountUp } from 'countup.js';
+
 
 
 
@@ -31,36 +34,23 @@ const changeSlideView = (slide) => {
         },
         breakpoints: {
             640: {
-              slidesPerView: 1,
-              spaceBetween: 20,
+                slidesPerView: 1,
+                spaceBetween: 30,
             },
             768: {
-              slidesPerView: 2,
-              spaceBetween: 40,
+                slidesPerView: 2,
+                spaceBetween: 30,
             },
             1024: {
-              slidesPerView: 3,
-              spaceBetween: 50,
+                slidesPerView: 3,
+                spaceBetween: 30,
             },
         }
     });
 }
 
 changeSlideView();
-// const mql = window.matchMedia("(max-width: 992px)");
 
-
-// const handleOrientationChange = (e) => {
-//     if (mql.matches) {
-//         changeSlideView(2);
-//     } else {
-//         changeSlideView(3);
-//     }
-
-
-   
-
-// }
 
 
 
@@ -80,60 +70,29 @@ changeSlideView();
 // };
 
 
-var btn = $('#button');
-
-$(window).scroll(function () {
-    if ($(window).scrollTop() > 300) {
-        btn.addClass('show');
-    } else {
-        btn.removeClass('show');
-    }
-});
-
-btn.on('click', function (e) {
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: 0 }, '300');
-});
 
 
-// scrollFnuction();
+// $(window).scroll(function () {
+//     if ($(window).scrollTop() > 300) {
+//         btn.addClass('show');
+//     } else {
+//         btn.removeClass('show');
+//     }
+// });
+
+// btn.on('click', function (e) {
+//     e.preventDefault();
+//     $('html, body').animate({ scrollTop: 0 }, '300');
+// });
 
 
-// window.onload = function () {
-//     const countUp = new CountUp('targetId', 5234);
-// if (!countUp.error) {
-//   countUp.start();
-// } else {
-//   console.error(countUp.error);
-// }
-//     // let website = new CountUp('website-count', 0, 50, 0, 2.5);
-//     // website.start();
-//     // let app = new CountUp('app-count', 0, 100, 0, 2.5);
-//     // app.start();
-//     // let user = new CountUp('user-count', 0, 1000, 0, 2.5);
-//     // user.start();
-// };
-
-
-const countUp = new CountUp('website-count', 50);
-if (!countUp.error) {
+window.onload = function () {
+    var countUp = new CountUp('website-count', 20);
     countUp.start();
-} else {
-    console.error(countUp.error);
-}
-
-const countUp1 = new CountUp('app-count', 100);
-if (!countUp1.error) {
+    var countUp1 = new CountUp('app-count', 100);
     countUp1.start();
-} else {
-    countUp1.error(countUp1.error);
-}
-const countUp2 = new CountUp('user-count', 700);
-if (!countUp2.error) {
+    var countUp2 = new CountUp('user-count', 700);
     countUp2.start();
-} else {
-    console.error(countUp2.error);
 }
-
 
 document.getElementById("year").innerHTML = "جميع الحقوق محفوظة  سنة " + new Date().getFullYear();
